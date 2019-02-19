a585387ee362071284cdb36a4ea83bf0fce4e82f
Patrick Winters <patrick.winters@gmail.com>
2019-02-19 11:30:54 -0500

    config needs prod env

--- config/config.js
+++ config/config.js
@@ -6,5 +6,13 @@ module.exports = {
       ssl: true
     },
     use_env_variable: "DATABASE_URL"
+  },
+  production: {
+    dialect: "postgres",
+    ssl: true,
+    dialectOptions: {
+      ssl: true
+    },
+    use_env_variable: "DATABASE_URL"
   }
 };
